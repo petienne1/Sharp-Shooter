47321e5a1a6521f98c7a508b23afaedefa2b8eef
Pierry Etienne <pierry.etienne@gmail.com>
2017-08-06 21:10:10 -0400

    Updated PlayerProfile component

--- Src/Client/App/Components/playerProfile.jsx
+++ Src/Client/App/Components/playerProfile.jsx
@@ -1,32 +1,11 @@
-// import React from 'react';
-//
-// class PlayerProfile extends React.Component {
-//
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showPlayerProfile: false
-//     },
-//   };
-// }
-//
-// render () {
-//   const playerProfile = this._getPlayerProfile();
-//   if(this.state.showPlayerProfile) {
-//
-//   }
-//   return (
-//     <h1>Player Info</h1>
-//     <h2>{currentPlayer.displayFirstLast}</h2>
-//     <h4>{"School: " + currentPlayer.school}</h4>
-//     <h4>{"Drafted: " + currentPlayer.draftYear}</h4>
-//     <h4>{"Team: " + currentPlayer.teamCity + ' ' + currentPlayer.teamName}</h4>
-//     <h4>{"Season: " + currentPlayer.seasonExp}</h4>
-//     <h4>{"Height: " +  currentPlayer.height}</h4>
-//     <h4>{"Weight: " + currentPlayer.weight}</h4>
-//     <h4>{"Position: " + currentPlayer.position}</h4>
-//     <h4>{"Jersey: " + currentPlayer.jersey}</h4>
-//   );
-// }
-//
-// export default PlayerProfile;
+import React from 'react';
+// import SweetScroll from 'sweet-scroll';
+
+const PlayerProfile = () => (
+  <div className="profile-container">
+    <div className="profile-pic-item" />
+    <div className="profile-info-item">{ object.data.commonPlayerInfo.displayFirstLast }</div>
+  </div>
+);
+
+export default PlayerProfile;
