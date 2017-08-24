919e8d5ae35acac280d38280a031b8f9b4260405
Pierry Etienne <pierry.etienne@gmail.com>
2017-08-24 18:37:28 -0400

    Added reset searchTerm to Input Form

--- Src/Client/App/Components/search.jsx
+++ Src/Client/App/Components/search.jsx
@@ -20,7 +20,8 @@ export default class Search extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.images = ['../../Style/images/shooters/allen.jpg', '../../Style/images/shooters/birdshot.jpg', '../../Style/images/shooters/jordanfinalshot.jpg', '../../Style/images/shooters/curry.jpg', '../../Style/images/shooters/durant.jpg'];
     this.randomImg = this.images[Math.floor(Math.random() * this.images.length)];
-    this.divStyle = { backgroundImage: 'url(' + this.randomImg + ')' }
+    this.divStyle = { backgroundImage: 'url(' + this.randomImg + ')' };
+
   }
 
   handleChange(event) {
@@ -34,7 +35,9 @@ export default class Search extends React.Component {
     const playerShots = axios.get(`/stats/shots?player=${this.state.searchTerm}`);
     playerObject.then((response) => {
       this.props.addPlayer(response.data);
-    });
+    }).then((res) => {
+      this.setState({searchTerm: ''});
+    })
     playerShots.then((response) => {
       this.props.addPlayer(response.data);
     });
@@ -53,39 +56,39 @@ export default class Search extends React.Component {
       <div className="main-container" style={this.divStyle}>
         <div className="input-wrapper">
           {/* <div className="header-info"> */}
-            <h1 className="app-name">Sharp Shooter</h1>
-            <form
-              className="form-group"
-              onSubmit={this.handleSubmit}
-            >
-              <input
-                className="input-field"
-                onChange={this.handleChange}
-                type="search"
-                placeholder="Search for a player..."
-                value={this.state.searchTerm}
-              />
-              <ul className="list-inline intro-buttons">
-                <li>
-                  <button
-                    type="submit"
-                    className="btn btn-default btn-lg"
-                    onSubmit={this.loadPlayerData}
-                  >
-                    <span className="button-name">Search</span>
-                  </button>
+          <h1 className="app-name">Sharp Shooter</h1>
+          <form
+            className="form-group"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              className="input-field"
+              onChange={this.handleChange}
+              type="search"
+              placeholder="Search for a player..."
+              value={this.state.searchTerm}
+            />
+            <ul className="list-inline intro-buttons">
+              <li>
+                <button
+                  type="submit"
+                  className="btn btn-default btn-lg"
+                  onSubmit={this.loadPlayerData}
+                >
+                  <span className="button-name">Search</span>
+                </button>
 
-                </li>
-                <li>
-                  <button className="btn btn-default btn-lg">
-                    <span className="button-name">Advanced</span></button>
-                </li>
-                <li>
-                  <button className="btn btn-default btn-lg">
-                    <span className="button-name">Compare</span></button>
-                </li>
-              </ul>
-            </form>
+              </li>
+              <li>
+                <button className="btn btn-default btn-lg">
+                  <span className="button-name">Advanced</span></button>
+              </li>
+              <li>
+                <button className="btn btn-default btn-lg">
+                  <span className="button-name">Compare</span></button>
+              </li>
+            </ul>
+          </form>
           {/* </div> */}
         </div>
         <div className="player-profile">{this.state.showPlayerProfile ?
