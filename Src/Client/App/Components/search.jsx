1c445f9c72f518268798b4bf2aea9acb30de56a8
Pierry Etienne <pierry.etienne@gmail.com>
2017-08-27 19:19:19 -0400

    updated player images

--- Src/Client/App/Components/search.jsx
+++ Src/Client/App/Components/search.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import SweetScroll from 'sweet-scroll';
+import Scroll from 'react-scroll';
 import PlayerProfile from './playerProfile.jsx';
 import App from './playerProfile.jsx';
 import Select from './select.jsx';
@@ -21,6 +22,7 @@ export default class Search extends React.Component {
     this.images = ['../../Style/images/shooters/allen.jpg', '../../Style/images/shooters/birdshot.jpg', '../../Style/images/shooters/jordanfinalshot.jpg', '../../Style/images/shooters/curry.jpg', '../../Style/images/shooters/durant.jpg'];
     this.randomImg = this.images[Math.floor(Math.random() * this.images.length)];
     this.divStyle = { backgroundImage: 'url(' + this.randomImg + ')' };
+    this.sweetScroll = this.sweetScroll.bind(this);
   }
 
   handleChange(event) {
@@ -42,6 +44,21 @@ export default class Search extends React.Component {
     });
   }
 
+  // sweetScroll() {
+  //   console.log('scrolling down');
+  //   const height = this.props.windowHeight - 62;
+  //   console.log([0, this.props.windowHeight - 62]);
+  //   sweetScroll.to(height, 0);
+  // }
+  componentDidMount() {
+    this.sweetScroll = new SweetScroll();
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.showPlayerProfile) {
+      this.sweetScroll.toElement(document.getElementById('player-profile'));
+    }
+  }
 
   // handleKeyUp(event) {
   //   if (event.key === 'Enter' && this.state.searchTerm !== '') {
@@ -72,7 +89,7 @@ export default class Search extends React.Component {
                 <button
                   type="submit"
                   className="btn btn-default btn-lg"
-                  onSubmit={this.loadPlayerData}
+                  onSubmit={this.loadPlayerData && this.sweetScroll}
                 >
                   <span className="button-name">Search</span>
                 </button>
